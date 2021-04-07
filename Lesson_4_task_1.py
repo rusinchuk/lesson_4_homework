938dd30ab1be30bccca62387682b99ee4892a248
rusinchuk <rusinchuk@gmail.com>
2021-04-07 19:56:30 +0300

    Homework_4_1 done. @elpiankova

---
+++ Lesson_4_task_1.py
@@ -0,0 +1,20 @@
+a = 355
+b = 'fgfsdfsf' # можно подставить численное значение и числа просуммируются
+try:
+    c = a + b
+except TypeError as c:
+    print(a, b)
+else:
+    print(c)
+
+
+
+
+
+
+
+
+
+
+
+
